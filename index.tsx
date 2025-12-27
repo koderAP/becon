6ab19516135ead8826fc0b84f8e7632be210a857
koderAP <apiitb01@gmail.com>
2025-12-27 22:24:44 +0530

    Fix: Properly wrapped App with AuthProvider

--- index.tsx
+++ index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-  </React.StrictMode >
+import './index.css';
+import { AuthProvider } from './src/contexts/AuthContext';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
